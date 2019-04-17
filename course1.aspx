5c9379bbf6c4bd31e19df9bb6d4740873c4e235b
hunnykumari <44862226+hunnykumari@users.noreply.github.com>
2019-04-17 15:22:01 +0530

    Add files via upload

---
+++ course1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site2.Master" AutoEventWireup="true" CodeBehind="course1.aspx.cs" Inherits="WebApplication2.course1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    Welcome to course Pages!!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <asp:TextBox ID="TextBox1" runat="server"  cssclass="txtbox" ></asp:TextBox>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Search" />
+                </p>
+<p>
+    Choose your preffered Skills to enhance your knowledge!<br />
+</p>
+<p>
+</p>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:db1ConnectionString3 %>" SelectCommand="SELECT * FROM [course]" ProviderName="System.Data.SqlClient"></asp:SqlDataSource>
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="courseCode" DataSourceID="SqlDataSource1" AllowPaging="True" AllowSorting="True" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" >
+                        <Columns>
+                            <asp:BoundField DataField="courseCode" HeaderText="courseCode" SortExpression="courseCode" ReadOnly="True" />
+                            <asp:BoundField DataField="courseDescription" HeaderText="courseDescription" SortExpression="courseDescription" />
+                          
+                            <asp:BoundField DataField="skillSet" HeaderText="skillSet" SortExpression="skillSet" />
+                            <asp:TemplateField HeaderText="Select">
+                                <ItemTemplate>
+                           <asp:HyperLink ID="l1" runat="server" Text="click" NavigateUrl='<%#Eval("link") %>'></asp:HyperLink>
+
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                          
+                        </Columns>
+                    </asp:GridView>
+<p>
+</p>
+</asp:Content>
---
+++ course1.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Configuration;
+
+namespace WebApplication2
+{
+    public partial class course1 : System.Web.UI.Page
+    { public SqlConnection con;
+        public string constr;
+
+        public void connection()
+        {
+            constr = ConfigurationManager.ConnectionStrings["db1ConnectionString3"].ToString();
+            con = new SqlConnection(constr);
+            con.Open();
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+         private void rep_bind()
+        {
+            connection();
+            string query = "select * from course where courseDescription like'" + TextBox1.Text + "%'";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSourceID = null;
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+           
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            connection();
+            string query = "select * from course where  courseDescription like'" + TextBox1.Text + "%'";
+            SqlCommand com = new SqlCommand(query, con);
+
+            SqlDataReader dr;
+            dr = com.ExecuteReader();
+
+
+            if (dr.HasRows)
+            {
+                dr.Read();
+
+                rep_bind();
+                GridView1.Visible = true;
+
+                TextBox1.Text = "";
+               // Label1.Text = "";
+            }
+            else
+            {
+                GridView1.Visible = false;
+              //  Label1.Visible = true;
+                Response.Write( "The search Term " + TextBox1.Text + " &nbsp;Is Not Available in the Records") ;
+
+            }
+         }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           GridViewRow dr=GridView1.SelectedRow;
+           if (dr.Cells[1].Text=="Programming Concept")
+           Response.Redirect("CSharpskill.aspx");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*try{
+             * SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["db1ConnectionString3"].ToString());
+             string query = "select * from course where courseDescription LIKE '" + TextBox1.Text + "'";
+             SqlCommand com = new SqlCommand(query, con);
+             con.Open();
+
+
+             SqlDataAdapter da = new SqlDataAdapter(query, con);
+             DataSet ds = new DataSet();
+             da.Fill(ds, "course");
+             GridView1.DataSourceID = null;
+
+
+
+             GridView1.DataSource = ds.Tables["course"];
+
+             SqlCommandBuilder comd = new SqlCommandBuilder(da);
+             da.Update(ds, "course");
+             ds.Tables["course"].AcceptChanges();} catch{}*/
+
+
+
+        }
+    }
